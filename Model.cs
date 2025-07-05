feb5927e95495b84cea0ab154f1943402e11e728
Jesus Castro <castrojesus39@gmail.com>
2025-07-04 22:32:55 -0700

    removed required from model

--- Model.cs
+++ Model.cs
@@ -1,14 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 
+namespace PhonebookProj;
+
 public class PersonContext : DbContext
 {
-    public required DbSet<Person> Person { get; set; }
+    public DbSet<Person> Person { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
             @"Server=(localdb)\Phonebook;Database=Phonebook;Trusted_Connection=True;"
-        );
+        ).LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
     }
 }
 
