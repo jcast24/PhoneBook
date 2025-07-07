51da81b2d1123ddbda9baf85ade7558fe1d1c19f
Jesus Castro <castrojesus39@gmail.com>
2025-07-06 22:07:25 -0700

    added two new rows and columns for the table output

--- UserInterface.cs
+++ UserInterface.cs
@@ -9,13 +9,16 @@ static internal class UserInterface
         var table = new Table();
         table.AddColumn("Id");
         table.AddColumn("Name");
+        table.AddColumn("Email");
+        table.AddColumn("Phone Number");
 
         foreach (var p in people)
         {
-            table.AddRow(p.Id.ToString(), p.Name);
+            table.AddRow(p.Id.ToString(), p.Name, p.Email, p.PhoneNumber);
         }
 
         AnsiConsole.Write(table);
+        Console.WriteLine("Press any key to continue!");
         Console.ReadLine();
     }
 }
