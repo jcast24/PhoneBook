e94be5a6f84338803c7a00eae0e434a1b1f5ab4c
Jesus Castro <castrojesus39@gmail.com>
2025-08-14 18:53:18 -0700

    Implemented the AddContact method

--- PhonebookController.cs
+++ PhonebookController.cs
@@ -9,4 +9,38 @@ public class PhonebookController
         var people = db.Person.ToList();
         return people;
     }
+
+    public Person? GetContactById(int id)
+    {
+        using var db = new PersonContext();
+        var person = db.Person.SingleOrDefault(x => x.Id == id);
+        return person;
+    }
+
+    public string AddContact()
+    {
+        using var db = new PersonContext();
+        var newPerson = new Person();
+
+        // Create a new person 
+        // add all their info 
+        // Create a method that parses both email and phone number into a correct format 
+        Console.WriteLine("Name: ");
+        string? name = Console.ReadLine();
+
+        Console.WriteLine("Phone Number: ");
+        string? phoneNumber = Console.ReadLine();
+
+        Console.WriteLine("Email: ");
+        string? email = Console.ReadLine();
+
+        newPerson.Name = name!;
+        newPerson.PhoneNumber = phoneNumber!;
+        newPerson.Email = email!;
+
+        db.Add(newPerson);
+        db.SaveChanges();
+
+        return "Succssfully added person.\n\n";
+    }
 }
